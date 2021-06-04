f6db725ac0f8855ea1e04ab04dc8920698cc948b
Miharu-380 <38t.ajlkk@gmail.com>
2021-06-04 00:54:26 +0000

    Add Favorites-ajax

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,28 @@
+class BookCommentsController < ApplicationController
+	before_action :authenticate_user!
+
+	def create
+		@book = Book.find(params[:book_id])
+		@book_comment = BookComment.new(book_comment_params)
+		@book_comment.book_id = @book.id
+		@book_comment.user_id = current_user.id
+		@book_comment.save
+		if @book_comment.save
+  		render 'books/create'
+		else
+		  render 'books/show'
+		end
+	end
+
+	def destroy
+		@book = Book.find(params[:book_id])
+  	book_comment = @book.book_comments.find(params[:id])
+		book_comment.destroy
+		render 'books/destroy'
+	end
+
+	private
+	def book_comment_params
+		params.require(:book_comment).permit(:comment)
+	end
+end
